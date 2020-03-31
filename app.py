dd66632a29a6b0248156f5d47b87ac2b9869df83
gabirk123 <62448908+gabirk123@users.noreply.github.com>
2020-04-01 00:16:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,23 +3,25 @@ from datetime import datetime
 from random import randint
 import time
 app = Flask(__name__)
+global color
+color = ''
 @app.route("/")
 def t():
-    color = ''
     name = 'gal'
     now = datetime.now()
     current = now.strftime("%H:%M:%S")
-    value = randint(0, 5)
-    if value == 1:
-        color = 'blue'
-    elif value == 2:
-        color = 'yellow'
-    elif value == 3:
-        color = 'green'
-    elif value == 4:
-        color = 'grey'
-    elif value == 5:
-        color = 'purple'
+    if color != '':
+        value = randint(0, 5)
+        if value == 1:
+            color = 'blue'
+        elif value == 2:
+            color = 'yellow'
+        elif value == 3:
+            color = 'green'
+        elif value == 4:
+            color = 'grey'
+        elif value == 5:
+            color = 'purple'
     return '''
        <html>
     <head>
